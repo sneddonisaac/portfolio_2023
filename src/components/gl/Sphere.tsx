a153a57d2d15d84f861c0647bb749e2735a2272d
Isaac <isaac@air.local>
2023-11-10 09:03:33 +0000

    qp

--- src/components/gl/Sphere.tsx
+++ src/components/gl/Sphere.tsx
@@ -1,5 +1,29 @@
 import { useEffect, useRef } from 'react';
 
+function generateSphereVertices(radius, latitudeBands, longitudeBands) {
+  const vertices = [];
+
+  for (let lat = 0; lat <= latitudeBands; lat++) {
+    const theta = (lat * Math.PI) / latitudeBands;
+    const sinTheta = Math.sin(theta);
+    const cosTheta = Math.cos(theta);
+
+    for (let long = 0; long <= longitudeBands; long++) {
+      const phi = (long * 2 * Math.PI) / longitudeBands;
+      const sinPhi = Math.sin(phi);
+      const cosPhi = Math.cos(phi);
+
+      const x = cosPhi * sinTheta;
+      const y = cosTheta;
+      const z = sinPhi * sinTheta;
+
+      vertices.push(radius * x, radius * y, radius * z);
+    }
+  }
+
+  return vertices;
+}
+
 const SphereCanvas = () => {
   const canvasRef = useRef(null);
 
@@ -10,19 +34,57 @@ const SphereCanvas = () => {
 
     // Vertex shader source code
     const vsSource = `
-      attribute vec4 a_position;
-      void main() {
-        gl_Position = a_position;
-        gl_PointSize = 3.0;
-      }
+     attribute vec3 a_position;
+     varying vec3 v_normal;
+
+     void main() {
+     	v_normal = a_position;
+     	gl_Position = projectionMatrix * modelViewMatrix * vec4(a_position, 1.0);
+     }    
     `;
 
     // Fragment shader source code
     const fsSource = `
-      void main() {
-        gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
-      }
+     precision mediump float;
+     varying vec3 v_normal;
+     uniform samplerCube u_environmentMap;
+
+     void main() {
+     	vec3 normal = normalize(v_normal);
+	vec3 reflection = reflect(normalize(-position), normal);
+	gl_FragColor = textureCube(u_environmentMap, reflection);
+     }
     `;
+
+    const envMap = gl.createTexture();
+    gl.bindTexture(gl.TEXTURE_CUBE_MAP, envMap);
+
+    const faces = [
+	    'posx.jpg', 'negx.jpg',
+	    'posy.jpg', 'negy.jpg',
+	    'posz.jpg', 'negz.jpg'
+    ];
+
+    faces.forEach((face, index) => {
+		    const image = new Image();
+		    image.src = face;
+		    image.onload = function() {
+		    gl.bindTexture(gl.TEXTURE_CUBE_MAP, envMap);
+		    gl.texImage2D(gl.TEXTURE_CUBE_MAP_POSITIVE_X + index, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
+		    }
+		    });
+
+    gl.texParameteri(gl.TEXTURE_CUBE_MAP, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_CUBE_MAP, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_CUBE_MAP, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+    gl.texParameteri(gl.TEXTURE_CUBE_MAP, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+
+    const u_environmentMap = gl.getUniformLocation(program, 'u_environmentMap');
+    gl.uniform1i(u_environmentMap, 0);
+    gl.activeTexture(gl.TEXTURE0);
+    gl.bindTexture(gl.TEXTURE_CUBE_MAP, envMap);
+
+
 
     // Create shaders
     const vertexShader = gl.createShader(gl.VERTEX_SHADER);
@@ -41,7 +103,7 @@ const SphereCanvas = () => {
     gl.useProgram(program);
 
     // Create vertex buffer
-    const vertices: any[] = []; // Fill this array with sphere vertices
+    const vertices = generateSphereVertices(1.0, 40, 40); // Fill this array with sphere vertices
     const vertexBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
@@ -51,7 +113,9 @@ const SphereCanvas = () => {
     gl.vertexAttribPointer(positionLocation, 3, gl.FLOAT, false, 0, 0);
 
     // Clear canvas and draw points
-    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.clearColor(0.0, 0.0, 0.0, 0.0);
+    gl.enable(gl.BLEND);
+    gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
     gl.clear(gl.COLOR_BUFFER_BIT);
     gl.drawArrays(gl.POINTS, 0, vertices.length / 3);
   }, []);
--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -1,17 +1,18 @@
 'use client';
 import { Canvas } from '@react-three/fiber';
 import Box from '@/components/gl/Box';
-import SphereCanvas from '@/components/gl/Sphere';
+import SphereComponent from '@/components/gl/Sphere2';
 
 export default function HomeView() {
   return (
     <main className='flex h-screen w-screen flex-col items-center justify-between overflow-hidden p-24'>
       <div
         className={
-          'fixed left-[50%] top-[50%] w-2/3 -translate-x-1/2 -translate-y-1/2 text-center text-5xl font-semibold'
+          'fixed w-full h-full text-center text-5xl font-semibold'
         }
+	style={{ left: '50%', top: '50%', transform: 'translate(-50%, -50%)' }}
       >
-        <SphereCanvas />
+        <SphereComponent radius={10} />
       </div>
       <div
         className={
